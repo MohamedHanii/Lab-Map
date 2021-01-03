831b259c13496a33bceaffe698bc302c7d4701ae
unknown <mohamed.hany556@gmail.com>
2021-01-03 23:30:09 +0200

    login/registration

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET login page. */
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* GET registration page. */
+router.get('/registration', function (req, res, next) {
+  res.render('signup', { title: 'Registration' });
 });
 
 module.exports = router;
